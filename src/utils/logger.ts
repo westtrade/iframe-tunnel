8e8a65ac8d1229ea9ff08cb8660263f21684aa0f
Gennadiy Popov <popov.gennadiy.87@gmail.com>
2020-05-18 18:18:05 +0300

    Update logger.ts

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,8 @@
 export function log(message: string) {
+  if (!window.debugTunnel) {
+    return 
+  }
+  
   const consoleProperty:string='console';
   if (window && window[consoleProperty]) {
     const cs = window[consoleProperty];
